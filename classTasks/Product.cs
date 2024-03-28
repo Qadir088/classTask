46f83cad009df2c93797b6c59c5e6b5a748e9ec3
Qadir088 <qadircabbarli55@gmail.com>
2024-03-28 12:55:18 +0400

    classTask

---
+++ classTasks/Product.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classTasks
+{
+    internal class Product
+    {
+        public int id;
+        public string BrandName;
+        public string Model;
+        public int Price;
+        public int Cost;
+        public int inCome;
+        public int Count;
+        public Product(int id, string brandName, string model, int price, int cost, int inCome, int count)
+        {
+            this.id = id;
+            BrandName = brandName;
+            Model = model;
+            Price = price;
+            Cost = cost;
+            this.inCome = inCome;
+            Count = count;
+            if(Count <0 )
+            {
+                Count =0;
+            }
+        }
+        public void getInfo()
+            {
+            Console.WriteLine($"{BrandName},{Model},{Price},{Cost},{Count}");
+        }
+    }
+}
---
+++ classTasks/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classTasks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product = new Product(1, "Koton", "shoes", 100, 100, 5,0);
+            product.getInfo();
+        }
+    }
+}
